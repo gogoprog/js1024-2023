b5cd85c210982e35326fffebb1606aa1203dd7b3
Gauthier Billot <gogoprog@gmail.com>
2023-07-07 13:24:31 +0200

    wip

--- src/Main.hx
+++ src/Main.hx
@@ -28,8 +28,8 @@ class MyShader extends ShaderMain {
 
     final lightPosition = vec3(0.0, 1.0, -5.0);
 
-    function v(x:Int, y:Int):Vec3 {
-        return vec3(x, y, 0);
+    function v2(x:Int):Vec3 {
+        return vec3(floor(x / 10), mod(x,10), 0);
     }
 
     function mainImage(fragColor:Vec4, fragCoord:Vec2):Void {
@@ -37,33 +37,32 @@ class MyShader extends ShaderMain {
         var ix = iResolution.x;
 
         var spherePositions:Array<Vec3, 22>;
-        final d = 1;
 
-        spherePositions[0] = v(d * 0, d * 2);
-        spherePositions[1] = v(d * 0, d * 1);
-        spherePositions[2] = v(d * 0, d * 0);
-        spherePositions[3] = v(d * 1, d * 0);
+        spherePositions[0] = v2(2);
+        spherePositions[1] = v2(1);
+        spherePositions[2] = v2(0);
+        spherePositions[3] = v2(10);
 
-        spherePositions[4] = v(d * 3, d * 2);
-        spherePositions[5] = v(d * 3, d * 1);
-        spherePositions[6] = v(d * 3, d * 0);
-        spherePositions[7] = v(d * 4, d * 0);
-        spherePositions[8] = v(d * 5, d * 0);
-        spherePositions[9] = v(d * 5, d * 1);
-        spherePositions[10] = v(d * 5, d * 2);
+        spherePositions[4] = v2(32);
+        spherePositions[5] = v2(31);
+        spherePositions[6] = v2(30);
+        spherePositions[7] = v2(40);
+        spherePositions[8] = v2(50);
+        spherePositions[9] = v2(51);
+        spherePositions[10] = v2(52);
 
-        spherePositions[11] = v(d * 7, d * 2);
-        spherePositions[12] = v(d * 8, d * 2);
-        spherePositions[13] = v(d * 7, d * 1);
-        spherePositions[14] = v(d * 7, d * 0);
-        spherePositions[15] = v(d * 8, d * 0);
+        spherePositions[11] = v2(72);
+        spherePositions[12] = v2(82);
+        spherePositions[13] = v2(71);
+        spherePositions[14] = v2(70);
+        spherePositions[15] = v2(80);
 
-        spherePositions[16] = v(d * 10, d * 2);
-        spherePositions[17] = v(d * 12, d * 2);
-        spherePositions[18] = v(d * 10, d * 1);
-        spherePositions[19] = v(d * 11, d * 1);
-        spherePositions[20] = v(d * 10, d * 0);
-        spherePositions[21] = v(d * 12, d * 0);
+        spherePositions[16] = v2(102);
+        spherePositions[17] = v2(122);
+        spherePositions[18] = v2(101);
+        spherePositions[19] = v2(111);
+        spherePositions[20] = v2(100);
+        spherePositions[21] = v2(120);
 
         var uv:Vec2;
         uv.x = (fragCoord.x - ix * 0.5) / ix;
@@ -84,7 +83,7 @@ class MyShader extends ShaderMain {
             for(i in 0...22) {
                 var sphere_position = spherePositions[i];
                 sphere_position.x -= 6;
-                sphere_position.z = -15 + sin((tt + i/9)* 5) * 1;
+                sphere_position.z = -15 + sin((tt + i*0.1)* 5) * 1;
                 var sphere_color = vec3(0.5, 0.2, sphere_position.y / 3);
                 var t = intersectSphere(origin, direction, sphere_position, sphere_radius);
 
@@ -133,7 +132,7 @@ class MyShader extends ShaderMain {
 
         var c = uv.xy * uv.xy * sin(uv.x * 5 + tt) * sin(uv.y * 7 + tt) + uv.x * sin(tt) * 0.3 + uv.y * sin(tt) * 0.2;
         var b = sqrt(abs(c.x + c.y) * 9) * 3;
-        fragColor = vec4(0.01, abs(sin(b)-sin(d)) /30, 0.1 + abs(sin(b)) /9, 1.0);
+        fragColor = vec4(0.01, abs(sin(b)) /30, 0.1 + abs(sin(b)) /9, 1.0);
 
         // if(uv.y > 0.5) {
         //     fragColor = vec4(1, 0, 0, 1);
